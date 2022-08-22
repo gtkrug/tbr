5f34bf005caa2d944d04ad47108997940ddf7423
Jeff Krug <jeff.krug@gtri.gatech.edu>
2022-08-22 18:09:09 -0400

    DB migration tweak for 1.3

--- grails-app/migrations/remove-orphan-records-from-provider_contact.groovy
+++ grails-app/migrations/remove-orphan-records-from-provider_contact.groovy
@@ -2,8 +2,8 @@ databaseChangeLog = {
 
     changeSet(author: "rs239 (generated)", id: "remove-orphan-records-from-provider-contact-8778059-1") {
 
-        sql("""DELETE binding_registry.provider_contact FROM binding_registry.provider_contact 
-                inner join binding_registry.provider on provider.id = provider_contact.provider_contacts_id
+        sql("""DELETE provider_contact FROM provider_contact 
+                inner join provider on provider.id = provider_contact.provider_contacts_id
                 inner join contact on contact_id = contact.id
                 where provider.organization_id != contact.organization_id""")
     }
